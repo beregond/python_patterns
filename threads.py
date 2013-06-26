104c00d43bf8ffba6ef12e0b6f428402769cc412
Szczepan Cieślik <szczepan.cieslik@gmail.com>
2013-06-26 23:28:03 +0200

    Fix: Threads solution and indentation.

--- threads.py
+++ threads.py
@@ -5,38 +5,52 @@ import thread, threading, time, random, hashlib
 # Semaphores
 r = threading.BoundedSemaphore()
 w = threading.BoundedSemaphore()
+c = threading.BoundedSemaphore()
+clients = 0
+event = threading.Event()
+event.set()
 
 def generateData():
-    data = hashlib.sha1()
-    data.update(str(random.random()))
-    return data.hexdigest()
+	data = hashlib.sha1()
+	data.update(str(random.random()))
+	return data.hexdigest()
 
 resource = generateData()
 
 def reader(name):
-    global resource
-    while True:
-        delay = random.randrange(1, 5)
-        time.sleep(delay)
-        with w, r:
-            print "Reader %s is reading resource: %s" % (name, resource)
+	global resource, clients, event
+	while True:
+		delay = random.randrange(1, 5)
+		time.sleep(delay)
+		with w:
+			event.clear()
+			with c:
+				clients += 1
+		with r:
+			print "Reader %s is reading resource: %s" % (name, resource)
+		with c:
+			clients -= 1
+			if clients == 0:
+				event.set()
 
 def writer(name):
-    global resource
-    while True:
-        delay = random.randrange(1, 5)
-        time.sleep(delay)
-        with w:
-            resource = generateData()
-            print "Writer %s changed data to %s" % (name, resource)
+	global resource, clients, event
+	while True:
+		delay = random.randrange(1, 5)
+		time.sleep(delay)
+		with w:
+			event.wait()
+			resource = generateData()
+			print "Writer %s changed data to %s" % (name, resource)
+			event.clear()
 
 try:
-    for i in range(1, 10):
-        thread.start_new_thread(reader, (i,))
-    for i in range(1, 2):
-        thread.start_new_thread(writer, (i,))
+	for i in range(1, 10):
+		thread.start_new_thread(reader, (i,))
+	for i in range(1, 2):
+		thread.start_new_thread(writer, (i,))
 except:
-    print "Error: Unable to start thread"
+	print "Error: Unable to start thread"
 
 while True:
-    pass
+	pass
