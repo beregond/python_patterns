7764ecbfe1d4fdd5a62c0f275ca5e9b760d80bd2
Szczepan Cieślik <szczepan.cieslik@gmail.com>
2013-07-06 13:23:27 +0200

    Modify: Improved threads example.

--- threads.py
+++ threads.py
@@ -2,53 +2,59 @@ import thread, threading, time, random, hashlib
 
 "Solution for classic readers-writers problem."
 
-# Semaphores
-r = threading.BoundedSemaphore()
-w = threading.BoundedSemaphore()
-c = threading.BoundedSemaphore()
-clients = 0
-event = threading.Event()
-event.set()
-
 def generateData():
 	data = hashlib.sha1()
 	data.update(str(random.random()))
 	return data.hexdigest()
 
-resource = generateData()
+class Library():
 
-def reader(name):
-	global resource, clients, event
+	def __init__(self):
+		self.event = threading.Event()
+		self.event.set()
+		self.c = threading.BoundedSemaphore()
+		self.w = threading.BoundedSemaphore()
+		self.r = threading.BoundedSemaphore()
+		self.clients = 0
+		self.resource = generateData()
+
+	def read(self, name):
+		with self.w:
+			self.event.clear()
+			with self.c:
+				self.clients += 1
+		with self.r:
+			print "Reader %s is reading resource: %s" % (name, self.resource)
+		with self.c:
+			self.clients -= 1
+			if self.clients == 0:
+				self.event.set()
+
+	def write(self, name):
+		with self.w:
+			self.event.wait()
+			self.resource = generateData()
+			print "Writer %s changed data to %s" % (name, self.resource)
+			self.event.clear()
+
+def reader(name, library):
 	while True:
 		delay = random.randrange(1, 5)
 		time.sleep(delay)
-		with w:
-			event.clear()
-			with c:
-				clients += 1
-		with r:
-			print "Reader %s is reading resource: %s" % (name, resource)
-		with c:
-			clients -= 1
-			if clients == 0:
-				event.set()
+		library.read(name)
 
-def writer(name):
-	global resource, clients, event
+def writer(name, library):
 	while True:
 		delay = random.randrange(1, 5)
 		time.sleep(delay)
-		with w:
-			event.wait()
-			resource = generateData()
-			print "Writer %s changed data to %s" % (name, resource)
-			event.clear()
+		library.write(name)
 
 try:
+	library = Library()
 	for i in range(1, 10):
-		thread.start_new_thread(reader, (i,))
+		thread.start_new_thread(reader, (i,library))
 	for i in range(1, 2):
-		thread.start_new_thread(writer, (i,))
+		thread.start_new_thread(writer, (i,library))
 except:
 	print "Error: Unable to start thread"
 
